061ccf82325a490b0f186d010266ed831dfa22aa
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-09-01 17:49:57 -0400

    first issues complete

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,12 +3,14 @@ import "./submit.js";
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
-import '../css/index.css'
+import '../css/index.css';
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { initdb } from './database';
 
 window.addEventListener('load', function () {
+    initdb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
-  });
+});
